febb9c79376fa31350e783a9041d3eb5a47e5185
ErrorExceptionSystems <error.exception.gaming@gmail.com>
2025-01-28 21:49:08 -0700

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,18 @@
-let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
 
-console.log(40 + 8 + 23 - 10);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'joan');
+
+javascriptIsFun = 'Yes';
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2025;
+console.log(typeof year, year);
+console.log(typeof null);
